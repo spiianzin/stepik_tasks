1ef222a1071a7d4945eeadf5b1634f6e54635c3b
Sergei_Piianzin <sergei_piianzin@epam.com>
2018-02-23 12:21:32 +0400

    Add task 2

--- NN/hw1_2.py
+++ NN/hw1_2.py
@@ -8,162 +8,15 @@ import numpy as np
 import random
 import time
 
+
 from functools import partial
 from numpy.linalg import norm
-
-random.seed(42)
+from math import fabs
 
 data = np.loadtxt("data.csv", delimiter=",")
 pears = data[:, 2] == 1
 apples = np.logical_not(pears)
 
-def create_perceptron(m):
-    """Создаём перцептрон со случайными весами и m входами"""
-    w = np.random.random((m, 1))
-    return Perceptron(w, 1)
-
-class Perceptron:
-
-    def __init__(self, w, b):
-        """
-        Инициализируем наш объект - перцептрон.
-        w - вектор весов размера (m, 1), где m - количество переменных
-        b - число
-        """
-        
-        self.w = w
-        self.b = b
-
-    def forward_pass(self, single_input):
-        """
-        Метод рассчитывает ответ перцептрона при предъявлении одного примера
-        single_input - вектор примера размера (m, 1).
-        Метод возвращает число (0 или 1) или boolean (True/False)
-        """
-        
-        result = 0
-        for i in range(0, len(self.w)):
-            result += self.w[i] * single_input[i]
-        result += self.b
-        
-        if result > 0:
-            return 1
-        else:
-            return 0
-
-    def vectorized_forward_pass(self, input_matrix):
-        """
-        Метод рассчитывает ответ перцептрона при предъявлении набора примеров
-        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
-        n - количество примеров, m - количество переменных
-        Возвращает вертикальный вектор размера (n, 1) с ответами перцептрона
-        (элементы вектора - boolean или целые числа (0 или 1))
-        """
-
-    	result = self.w.T.dot(input_matrix) + self.b
-    	result = result > 0
-    	return result
-    
-    def train_on_single_example(self, example, y):
-        """
-        принимает вектор активации входов example формы (m, 1) 
-        и правильный ответ для него (число 0 или 1 или boolean),
-        обновляет значения весов перцептрона в соответствии с этим примером
-        и возвращает размер ошибки, которая случилась на этом примере до изменения весов (0 или 1)
-        (на её основании мы потом построим интересный график)
-        """
-
-    	result = int(self.vectorized_forward_pass(example))
-
-        error = y - result
-        lam = error * example
-
-        self.w += lam
-        self.b += error 
-
-        return error
-
-    
-    def train_until_convergence(self, input_matrix, y, max_steps=1e8):
-        """
-        input_matrix - матрица входов размера (n, m),
-        y - вектор правильных ответов размера (n, 1) (y[i] - правильный ответ на пример input_matrix[i]),
-        max_steps - максимальное количество шагов.
-        Применяем train_on_single_example, пока не перестанем ошибаться или до умопомрачения.
-        Константа max_steps - наше понимание того, что считать умопомрачением.
-        """
-        i = 0
-        errors = 1
-        while errors and i < max_steps:
-            i += 1
-            errors = 0
-            for example, answer in zip(input_matrix, y):
-                example = example.reshape((example.size, 1))
-                error = self.train_on_single_example(example, answer)
-                errors += int(error) 
-
-def plot_line(coefs):
-    """
-    рисует разделяющую прямую, соответствующую весам, переданным в coefs = (weights, bias), 
-    где weights - ndarray формы (2, 1), bias - число
-    """
-
-    w, bias = coefs
-    a, b = - w[0][0] / w[1][0], - bias / w[1][0]
-    xx = np.linspace(*plt.xlim())
-    line.set_data(xx, a*xx + b)
-
-def step_by_step_weights(p, input_matrix, y, max_steps=1e6):
-    """
-    обучает перцептрон последовательно на каждой строчке входных данных, 
-    возвращает обновлённые веса при каждом их изменении
-    p - объект класса Perceptron
-    """
-    i = 0
-    errors = 1
-    while errors and i < max_steps:
-        i += 1
-        errors = 0
-        for example, answer in zip(input_matrix, y):
-            example = example.reshape((example.size, 1))
-            
-            error = p.train_on_single_example(example, answer)
-            errors += error  # здесь мы упадём, если вы забыли вернуть размер ошибки из train_on_single_example
-            if error: 
-                # будем обновлять положение линии только тогда, когда она изменила своё положение
-                yield p.w, p.b
-                
-    for _ in range(20): yield p.w, p.b
-
-np.random.seed(1)
-fig = plt.figure()
-plt.scatter(data[apples][:, 0], data[apples][:, 1], color = "red", marker=".", label="Apples")
-plt.scatter(data[pears][:, 0], data[pears][:, 1], color = "green", marker=".", label="Pears")
-plt.xlabel("yellowness")
-plt.ylabel("symmetry")
-line, = plt.plot([], [], color="black", linewidth=2)  # создаём линию, которая будет показывать границу разделения
-
-from matplotlib.animation import FuncAnimation
-
-perceptron_for_weights_line = create_perceptron(2)  # создаём перцептрон нужной размерности со случайными весами
-
-from functools import partial
-weights_ani = partial(
-    step_by_step_weights, p=perceptron_for_weights_line, input_matrix=data[:, :-1], y=data[:, -1][:,np.newaxis]
-)  # про partial почитайте на https://docs.python.org/3/library/functools.html#functools.partial
-
-ani = FuncAnimation(fig, func=plot_line, frames=weights_ani, blit=False, interval=10, repeat=False)
-# если Jupyter не показывает вам анимацию - раскомментируйте строчку ниже и посмотрите видео
-# ani.save("perceptron_seeking_for_solution.mp4", fps=15)
-plt.show()
-
-
-
-############################################3
-#############################################
-############################################
-
-## Определим разные полезные функции
 
 def sigmoid(x):
     """сигмоидальная функция, работает и с числами, и с векторами (поэлементно)"""
@@ -175,52 +28,52 @@ def sigmoid_prime(x):
 
 
 class Neuron:
-    
+
     def __init__(self, weights, activation_function=sigmoid, activation_function_derivative=sigmoid_prime):
         """
         weights - вертикальный вектор весов нейрона формы (m, 1), weights[0][0] - смещение
         activation_function - активационная функция нейрона, сигмоидальная функция по умолчанию
         activation_function_derivative - производная активационной функции нейрона
         """
-        
+
         assert weights.shape[1] == 1, "Incorrect weight shape"
-        
+
         self.w = weights
         self.activation_function = activation_function
         self.activation_function_derivative = activation_function_derivative
-        
+
     def forward_pass(self, single_input):
         """
         активационная функция логистического нейрона
-        single_input - вектор входов формы (m, 1), 
+        single_input - вектор входов формы (m, 1),
         первый элемент вектора single_input - единица (если вы хотите учитывать смещение)
         """
-        
+
         result = 0
         for i in range(self.w.size):
             result += float(self.w[i] * single_input[i])
         return self.activation_function(result)
-    
+
     def summatory(self, input_matrix):
         """
-        Вычисляет результат сумматорной функции для каждого примера из input_matrix. 
+        Вычисляет результат сумматорной функции для каждого примера из input_matrix.
         input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
         n - количество примеров, m - количество переменных.
         Возвращает вектор значений сумматорной функции размера (n, 1).
         """
         return input_matrix.dot(self.w)
-    
+
     def activation(self, summatory_activation):
         """
         Вычисляет для каждого примера результат активационной функции,
         получив на вход вектор значений сумматорной функций
-        summatory_activation - вектор размера (n, 1), 
+        summatory_activation - вектор размера (n, 1),
         где summatory_activation[i] - значение суммматорной функции для i-го примера.
-        Возвращает вектор размера (n, 1), содержащий в i-й строке 
+        Возвращает вектор размера (n, 1), содержащий в i-й строке
         значение активационной функции для i-го примера.
         """
         return sigmoid(summatory_activation)
-    
+
     def vectorized_forward_pass(self, input_matrix):
         """
         Векторизованная активационная функция логистического нейрона.
@@ -230,53 +83,61 @@ class Neuron:
         (элементы вектора - float)
         """
         return self.activation(self.summatory(input_matrix))
-        
+
     def SGD(self, X, y, batch_size, learning_rate=0.1, eps=1e-6, max_steps=200):
         """
         Внешний цикл алгоритма градиентного спуска.
         X - матрица входных активаций (n, m)
         y - вектор правильных ответов (n, 1)
-        
+
         learning_rate - константа скорости обучения
-        batch_size - размер батча, на основании которого 
+        batch_size - размер батча, на основании которого
         рассчитывается градиент и совершается один шаг алгоритма
-        
-        eps - критерий остановки номер один: если разница между значением целевой функции 
-        до и после обновления весов меньше eps - алгоритм останавливается. 
+
+        eps - критерий остановки номер один: если разница между значением целевой функции
+        до и после обновления весов меньше eps - алгоритм останавливается.
         Вторым вариантом была бы проверка размера градиента, а не изменение функции,
         что будет работать лучше - неочевидно. В заданиях используйте первый подход.
-        
-        max_steps - критерий остановки номер два: если количество обновлений весов 
+
+        max_steps - критерий остановки номер два: если количество обновлений весов
         достигло max_steps, то алгоритм останавливается
-        
-        Метод возвращает 1, если отработал первый критерий остановки (спуск сошёлся) 
+
+        Метод возвращает 1, если отработал первый критерий остановки (спуск сошёлся)
         и 0, если второй (спуск не достиг минимума за отведённое время).
         """
 
 
 
-        
+
         # Этот метод необходимо реализовать
-        
+
         pass
-    
+
     def update_mini_batch(self, X, y, learning_rate, eps):
         """
         X - матрица размера (batch_size, m)
         y - вектор правильных ответов размера (batch_size, 1)
         learning_rate - константа скорости обучения
-        eps - критерий остановки номер один: если разница между значением целевой функции 
-        до и после обновления весов меньше eps - алгоритм останавливается. 
-        
-        Рассчитывает градиент (не забывайте использовать подготовленные заранее внешние функции) 
-        и обновляет веса нейрона. Если ошибка изменилась меньше, чем на eps - возвращаем 1, 
+        eps - критерий остановки номер один: если разница между значением целевой функции
+        до и после обновления весов меньше eps - алгоритм останавливается.
+
+        Рассчитывает градиент (не забывайте использовать подготовленные заранее внешние функции)
+        и обновляет веса нейрона. Если ошибка изменилась меньше, чем на eps - возвращаем 1,
         иначе возвращаем 0.
         """
-        
-        
 
-        
-        pass
+        J_n = compute_grad_analytically(self, X, y)
+        old = J_quadratic(self, X, y)
+        self.w += J_n * (-learning_rate)
+        after = J_quadratic(self, X, y)
+        if (fabs(old - after) < eps) :
+            return 1
+        else :
+            return 0
+
+
+
+
 
 def J_quadratic(neuron, X, y):
     """
@@ -286,12 +147,12 @@ def J_quadratic(neuron, X, y):
     neuron - нейрон, у которого есть метод vectorized_forward_pass, предсказывающий значения на выборке X
     X - матрица входных активаций (n, m)
     y - вектор правильных ответов (n, 1)
-        
+
     Возвращает значение J (число)
     """
-    
+
     assert y.shape[1] == 1, 'Incorrect y shape'
-    
+
     return 0.5 * np.mean((neuron.vectorized_forward_pass(X) - y) ** 2)
 
 def J_quadratic_derivative(y, y_hat):
@@ -299,17 +160,17 @@ def J_quadratic_derivative(y, y_hat):
     Вычисляет вектор частных производных целевой функции по каждому из предсказаний.
     y_hat - вертикальный вектор предсказаний,
     y - вертикальный вектор правильных ответов,
-    
-    В данном случае функция смехотворно простая, но если мы захотим поэкспериментировать 
+
+    В данном случае функция смехотворно простая, но если мы захотим поэкспериментировать
     с целевыми функциями - полезно вынести эти вычисления в отдельный этап.
-    
+
     Возвращает вектор значений производной целевой функции для каждого примера отдельно.
     """
-    
+
     assert y_hat.shape == y.shape and y_hat.shape[1] == 1, 'Incorrect shapes'
-    
+
     return (y_hat - y) / len(y)
-    
+
 def compute_grad_analytically(neuron, X, y, J_prime=J_quadratic_derivative):
     """
     Аналитическая производная целевой функции
@@ -317,31 +178,46 @@ def compute_grad_analytically(neuron, X, y, J_prime=J_quadratic_derivative):
     X - вертикальная матрица входов формы (n, m), на которой считается сумма квадратов отклонений
     y - правильные ответы для примеров из матрицы X
     J_prime - функция, считающая производные целевой функции по ответам
-    
+
     Возвращает вектор размера (m, 1)
     """
-    
+
     # Вычисляем активации
     # z - вектор результатов сумматорной функции нейрона на разных примерах
-    
+
     z = neuron.summatory(X)
     y_hat = neuron.activation(z)
 
     # Вычисляем нужные нам частные производные
     dy_dyhat = J_prime(y, y_hat)
     dyhat_dz = neuron.activation_function_derivative(z)
-    
+
     # осознайте эту строчку:
     dz_dw = X
 
     # а главное, эту:
     grad = ((dy_dyhat * dyhat_dz).T).dot(dz_dw)
-    
+
     # можно было написать в два этапа. Осознайте, почему получается одно и то же
     # grad_matrix = dy_dyhat * dyhat_dz * dz_dw
     # grad = np.sum(, axis=0)
-    
+
     # Сделаем из горизонтального вектора вертикальный
     grad = grad.T
-    
+
     return grad
+
+
+
+np.random.seed(42)
+n = 10
+m = 5
+
+X = 20 * np.random.sample((n, m)) - 10
+y = (np.random.random(n) < 0.5).astype(np.int)[:, np.newaxis]
+w = 2 * np.random.random((m, 1)) - 1
+
+neuron = Neuron(w)
+neuron.update_mini_batch(X, y, 0.1, 1e-5)
+
+print(neuron.w)
